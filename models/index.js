0a84ae00e05455a98c7cfcaaa07ba252ff1a9714
Unknown <bpmjg3@gmail.com>
2018-03-01 14:55:04 -0800

    Merge branch 'master' into matt

--- models/index.js
+++ models/index.js
@@ -9,11 +9,29 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 
+
+// for config.json
+// "development": {
+//   "username": "s0y0t7xx9tbeq4jx",
+//     "password": "qf1a2gybn52s0geu",
+//       "database": "worklife_db",
+//         "host": "z37udk8g6jiaqcbx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//           "dialect": "mysql"
+// },
+
+console.log(config.use_env_variable);
+
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+// //local host
+// var sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+
 
 fs
   .readdirSync(__dirname)
