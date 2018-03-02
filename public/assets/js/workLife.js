559bc8508eaa7b3343af544570ef24f7bbecec3e
Daniel Ward <dward1502@gmail.com>
2018-03-02 13:29:19 -0800

    added information to handlebars

--- public/assets/js/workLife.js
+++ public/assets/js/workLife.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $("#loginModal").hide();
-
 });
 
 
@@ -10,13 +9,55 @@ $('#login').leanModal({
     closeButton: ".close"
 });
 
+$("#loginBtn").on("click", function (event) {
+    console.log("login btn works");
 
+    var userLogin = {
+        username: $("#username").val().trim(),
+        password: $("#password").val().trim()
+    };
+    console.log(userLogin);
+
+
+    $.ajax("/login", {
+        type: "POST",
+        data: userLogin
+    }).then(function () {
+        console.log("username has been sent to server");
+    });
+
+    event.preventDefault();
+});
+
+$("#submit").on("click", function (event) {
+    console.log("register btn works");
+
+    var newRegUser = {
+        firstname: $("#firstName").val().trim(),
+        lastname: $("#lastName").val().trim(),
+        username: $("#userName").val().trim(),
+        password: $("#password").val().trim()
+    }
+    console.log(newRegUser);
+
+    var radioCheck = $('.mar:checked').val();
+    console.log("This is the radio button value if it is checked: " + radioCheck);
+
+
+
+});
+
+
+var w = window.innerWidth;
+var h = window.innerHeight;
 //Javascript for cool animation on page
 var container = document.getElementById('container');
 var renderer = new FSS.SVGRenderer();
 var scene = new FSS.Scene();
-var light = new FSS.Light('#111122', '#FF0022');
-var geometry = new FSS.Plane(1400, 1200, 6, 4);
+//             colors      secondary     main 
+var light = new FSS.Light('#111122', '#7ddc1f');
+//                         w     h   #of polygon  angle of polygon
+var geometry = new FSS.Plane(1400, 1200, 10, 6);
 var material = new FSS.Material('#FFFFFF', '#FFFFFF');
 var mesh = new FSS.Mesh(geometry, material);
 var now, start = Date.now();
